c8e56cebe677b6d7535656563b0def192049fa63
Davide Lo Re <boyska@gmail.com>
2010-03-16 09:46:02 +0100

    FIX functions

--- after.sh
+++ after.sh
@@ -1,11 +1,11 @@
-wait_job() {
+function wait_job() {
   echo 'looking for job' $1
   job_pid=$(jobs -p $1)
   echo 'It is' $job_pid 
   python after.py $job_pid '$2'
 }
 
-wait_pname() {
+function wait_pname() {
   proc_pid=$(pgrep "$*")
   for pid in $proc_pid; do
 	python after.py '$pid'
